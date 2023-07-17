5e92e858fe3ccc5418a9c9c3fd829534c1e6fa6b
irene <panisirene@gmail.com>
2023-07-17 13:43:14 -0700

    add GET reqs to render individual posts and login page

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { Post, Comment } = require('../models');
 
+// render homepage - contains all user posts
 router.get('/', async (req, res) => {
   try {
     const dbPostData = await Post.findAll({
@@ -22,6 +23,7 @@ router.get('/', async (req, res) => {
   }
 });
 
+// render dashboard - contains personal user posts
 router.get('/dashboard', async (req, res) => {
   try {
     const dbUserPostData = await Post.findAll({
@@ -47,4 +49,27 @@ router.get('/dashboard', async (req, res) => {
   } catch (err) {
     res.status(500).json(err);
   }
-})
+});
+
+router.get('/post/:id', async (req, res) => {
+  try {
+    const dbPostData = await Post.findByPk(req.params.id);
+    const post = dbPostData.get({ plain: true });
+    res.render('post', {
+      post,
+      username: req.session.username,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// renders login page
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+  res.render('login');
+});
