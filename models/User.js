99c6ab535899a574edd7e8a74c7e4f9025385d54
irene <panisirene@gmail.com>
2023-07-17 15:58:08 -0700

    fix file path

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 const { Model, DataTypes } = require('sequelize');
-const sequelize = require('..config/connection');
+const sequelize = require('../config/connection');
 // incorporate bcrypt and password compare/hash later
 
 class User extends Model {}
