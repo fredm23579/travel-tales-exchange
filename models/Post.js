68880d7a0664ee36f1082160f687efa5ba1b26b2
irene <panisirene@gmail.com>
2023-07-17 09:20:23 -0700

    add post model

---
+++ models/Post.js
@@ -0,0 +1,38 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('..config/connection');
+
+class Post extends Model {}
+
+Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    creator: {
+      type: DataTypes.STRING,
+      references: {
+        model: 'user',
+        key: 'username',
+      }
+    }
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+  }
+);
+
+module.exports = User;
