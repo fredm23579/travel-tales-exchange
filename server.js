3c011e5d73f59ef14fb4c3541e926ad203559be9
irene <panisirene@gmail.com>
2023-07-18 17:25:12 -0700

    add method override package

--- server.js
+++ server.js
@@ -2,14 +2,17 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
+const methodOverride = require('method-override'); // for handling PUT requests
 // Initializes Sequelize with session store
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+app.use(methodOverride('_method'));
 
 // Sets up session and connect to our Sequelize db
 const sess = {
